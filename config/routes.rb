86ce0de115eea12f351d9be2e8f78f2e3e1a7ed6
Reagan <rokhanga@gmail.com>
2023-07-06 12:45:19 +0300

    Define hero powers route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :powers, only: [:index, :show, :update]
   resources :heros, only: [:index, :show]
+  resources :hero_powers, only: [:create]
 end
