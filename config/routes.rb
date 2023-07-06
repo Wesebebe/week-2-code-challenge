764cb4b3948d947d396055e0793becf4acf5d01b
Reagan <rokhanga@gmail.com>
2023-07-06 12:41:46 +0300

    Define route to heros

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :powers, only: [:index, :show, :update]
+  resources :heros, only: [:index, :show]
 end
